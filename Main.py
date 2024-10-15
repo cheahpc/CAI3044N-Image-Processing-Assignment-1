eaacf4547ef9df8cfd364c423c7d2e7103c62099
Cheah Pin Chee <mr.chee37@gmail.com>
2024-10-15 15:28:44 +0800

    Amended saveSet to accept single string fileName Modified to assignment requirements, changing image paths. Updated Compression Ratio Calculation Modified gaussian filter parameter to single kernel size Reduced concat set function WIP

--- Main.py
+++ Main.py
@@ -2,172 +2,153 @@ import func as f
 import cv2
 
 # Settings
-IMG_NO = 2
-READ_SET = False
-SAVE_NEW_IMAGE = False
+SAVE_NEW_GTI = False
+SAVE_NEW_PROCESSED = True
+SCALE_FACTOR = 0.25
 
-SCALE_FACTOR = 0.15
 
-# 1. Average filter
-# 2. Box filter
-# 3. Gaussian filter
-# 4. Median filter
-# 5. Bilateral filter
-# 6. Non-local means filter
-# 7. Custom kernel filter
-SMOOTH_TYPE = 7
-# Parameters for smoothing filter
-SMOOTH_KERNEL_SIZE_X = 3
-SMOOTH_KERNEL_SIZE_Y = 7
-SMOOTH_KERNEL_SIZE = 5
-SMOOTH_SIGMA_X = 0  # For gaussian filter
-SMOOTH_SIGMA_Y = 0  # For gaussian filter
-SMOOTH_D = 9  # For bilateral filter
-SMOOTH_SIGMA_COLOR = 75  # For bilateral filter
-SMOOTH_SIGMA_SPACE = 75  # For bilateral filter
-SMOOTH_H = 10  # For non-local means filter
-SMOOTH_SEARCH_WINDOW_SIZE = 20  # For non-local means filter
+path_raw_h = "img/raw_h_1.NEF"
+path_raw_v = "img/raw_v_2.NEF"
 
-pathRawImage = [
-    "img/raw_v_1.NEF",
-    "img/raw_v_2.NEF",
-    "img/raw_v_3.NEF",
-    "img/raw_v_4.NEF",
-    "img/raw_h_1.NEF",
-    "img/raw_h_2.NEF",
-    "img/raw_h_3.NEF",
-    "img/raw_h_4.NEF",
-]
+path_gti_h = "img/gti_h.TIFF"  # Gray Scaled
+path_gti_v = "img/gti_v.TIFF"  # Gray Scaled
 
-pathGTIGrayScaled = [
-    "img/gti_gray_v_1.TIFF",
-    "img/gti_gray_v_2.TIFF",
-    "img/gti_gray_v_3.TIFF",
-    "img/gti_gray_v_4.TIFF",
-    "img/gti_gray_h_1.TIFF",
-    "img/gti_gray_h_2.TIFF",
-    "img/gti_gray_h_3.TIFF",
-    "img/gti_gray_h_4.TIFF",
-]
-
-pathGTIRGBScaled = [
-    "img/gti_rgb_v_1.tiff",
-    "img/gti_rgb_v_2.tiff",
-    "img/gti_rgb_v_3.tiff",
-    "img/gti_rgb_v_4.tiff",
-    "img/gti_rgb_h_1.tiff",
-    "img/gti_rgb_h_2.tiff",
-    "img/gti_rgb_h_3.tiff",
-    "img/gti_rgb_h_4.tiff",
-]
-
-# Canny parameters
-cannyParamSet = [
-    {"tresh1": 100, "tresh2": 200, "apertureSize": 3, "L2gradient": True},  #
-    {"tresh1": 100, "tresh2": 200, "apertureSize": 3, "L2gradient": False},  #
-    {"tresh1": 100, "tresh2": 200, "apertureSize": 5, "L2gradient": True},  #
-    {"tresh1": 100, "tresh2": 200, "apertureSize": 5, "L2gradient": False},  #
-]
-
-# Laplacian parameters
-laplacianParamSet = [
-    {
-        "ddepth": cv2.CV_16S,
-        "ksize": 3,
-        "scale": 1,
-        "delta": 0,
-        "borderType": cv2.BORDER_DEFAULT,
-    },  #
-    {
-        "ddepth": cv2.CV_16S,
-        "ksize": 3,
-        "scale": 1,
-        "delta": 0,
-        "borderType": cv2.BORDER_DEFAULT,
-    },  #
-]
 
 # =========================================================================================================== MAIN CODE
 
 # Step 1: Read RAW Image ====================================================================================
-raw = [f.Converter.raw2bgr(pathRawImage[IMG_NO]), f.Converter.raw2bgr(pathRawImage)][READ_SET]  # 3 channel image, 3 dimension (Height, Width, Channel)
+raw_h = f.Converter.raw2bgr(path_raw_h)  # 3 channel
+raw_v = f.Converter.raw2bgr(path_raw_v)  # 3 channel
 
 # Step 2: Convert to Gray ===================================================================================
-rawGray = f.Converter.bgr2gray(raw)  # Convert to gray, 1 channel image, 2 dimension (Height, Width)
+raw_h_gray = f.Converter.bgr2gray(raw_h)  # Convert to gray, 1 channel
+raw_v_gray = f.Converter.bgr2gray(raw_v)  # Convert to gray, 1 channel
 
 # Step 3: Scale the image ===================================================================================
-rawGrayScaled = f.Image.scaleSetBy(rawGray, SCALE_FACTOR)  # 1 channel image, 2 dimension (Height, Width)
-rawScaled = f.Image.scaleSetBy(raw, SCALE_FACTOR)  # Scale to new size RGB
+raw_h_gray_scaled = f.Image.scaleSetBy(raw_h_gray, SCALE_FACTOR)  # 1 channel, scaled
+raw_v_gray_scaled = f.Image.scaleSetBy(raw_v_gray, SCALE_FACTOR)  # 1 channel, scaled
 
 # Save as TIFF for GTI (Ground Truth Image)
 # -------------------------------------------------------------------------------------------------------------------------- SAVE
-if SAVE_NEW_IMAGE:
-    f.Image.saveSet(rawGrayScaled, pathGTIGrayScaled, True)
-    f.Image.saveSet(rawScaled, pathGTIRGBScaled)
+if SAVE_NEW_GTI:
+    f.Image.saveSet(raw_h_gray_scaled, path_gti_h)
+    f.Image.saveSet(raw_v_gray_scaled, path_gti_v)
 
 # Step 4: Read GTI Image ====================================================================================
-gtiGray = [f.Image.readSet(pathGTIGrayScaled[IMG_NO], True), f.Image.readSet(pathGTIGrayScaled, True)][READ_SET]
-gtiRGB = [f.Image.readSet(pathGTIRGBScaled[IMG_NO]), f.Image.readSet(pathGTIRGBScaled)][READ_SET]
+gti_h = f.Image.readSet(path_gti_h, True)  # Read image as gray scaled
+gti_v = f.Image.readSet(path_gti_v, True)  # Read image as gray scaled
 
-# Step 5: Apply blur Smooth Filter ========================================================================
-sfAverageRGB = f.SmoothFilter.applyAverageFilter(gtiRGB[IMG_NO], SMOOTH_KERNEL_SIZE_X, SMOOTH_KERNEL_SIZE_Y)
-sfGaussianRGB1 = f.SmoothFilter.applyGaussianBlurFilter(gtiRGB[IMG_NO], 3, 3, 0, 0)
-sfGaussianRGB2 = f.SmoothFilter.applyGaussianBlurFilter(gtiRGB[IMG_NO], 11, 11, 0, 0)
-sfGaussianRGB3 = f.SmoothFilter.applyGaussianBlurFilter(gtiRGB[IMG_NO], 3, 3, 25, 25)
-sfBilateralRGB1 = f.SmoothFilter.applyBilateralFilter(gtiRGB[IMG_NO], 9, 100, 100)
-sfBilateralRGB2 = f.SmoothFilter.applyBilateralFilter(gtiRGB[IMG_NO], 11, 75, 75)
 
-# ----------------------------------------------------------------------------------------------------------- Show
-print("gtiGray image: ", gtiGray.shape, " | ", f.Image.getImageType(gtiGray))
-print("gtiRGB image: ", gtiRGB.shape, " | ", f.Image.getImageType(gtiRGB))
+# Step 5: Apply Smooth Filter ===============================================================================
+# Apply Gaussian
+sg_h_1 = f.SmoothFilter.applyGaussianBlurFilter(gti_h, 3, 0, 0)
+sg_h_2 = f.SmoothFilter.applyGaussianBlurFilter(gti_h, 5, 0, 0)
+sg_h_3 = f.SmoothFilter.applyGaussianBlurFilter(gti_h, 7, 0, 0)
+sg_h_11 = f.SmoothFilter.applyGaussianBlurFilter(gti_h, 3, 5, 5)
+sg_h_12 = f.SmoothFilter.applyGaussianBlurFilter(gti_h, 3, 20, 20)
+sg_h_13 = f.SmoothFilter.applyGaussianBlurFilter(gti_h, 3, 50, 50)
 
-# Show Image
-cv2.imshow("gtiGray", gtiGray)
-cv2.imshow("gtiRGB", gtiRGB)
+sg_v_1 = f.SmoothFilter.applyGaussianBlurFilter(gti_v, 3, 0, 0)
+sg_v_2 = f.SmoothFilter.applyGaussianBlurFilter(gti_v, 5, 0, 0)
+sg_v_3 = f.SmoothFilter.applyGaussianBlurFilter(gti_v, 7, 0, 0)
+sg_v_11 = f.SmoothFilter.applyGaussianBlurFilter(gti_v, 3, 5, 5)
+sg_v_12 = f.SmoothFilter.applyGaussianBlurFilter(gti_v, 3, 20, 20)
+sg_v_13 = f.SmoothFilter.applyGaussianBlurFilter(gti_v, 3, 50, 50)
+
+# Apply Bilateral
+
+
+# Step 6: Apply Edge Filter ===============================================================================
+# canny1 = f.EdgeFilter.applyCanny(gti_h, cannyParamSet[0])
+# canny2 = f.EdgeFilter.applyCanny(gti_v, cannyParamSet[0])
+
+# Step 5: Compute Peak Signal-to-Noise Ratio (PSNR) and compression ratio ===================================
+# PSNR - Smoothed
+psnr_sg_h_1 = f.Compute.peakSignalToNoiseRatio(gti_h, sg_h_1)
+psnr_sg_h_2 = f.Compute.peakSignalToNoiseRatio(gti_h, sg_h_2)
+psnr_sg_h_3 = f.Compute.peakSignalToNoiseRatio(gti_h, sg_h_3)
+
+psnr_sg_v_1 = f.Compute.peakSignalToNoiseRatio(gti_v, sg_v_1)
+psnr_sg_v_2 = f.Compute.peakSignalToNoiseRatio(gti_v, sg_v_2)
+psnr_sg_v_3 = f.Compute.peakSignalToNoiseRatio(gti_v, sg_v_3)
+
+
+# Compression Ratio - RAW & GTI
+cr_raw_gti_h = f.Compute.compressionRatio(raw_h, gti_h, 8)
+cr_raw_gti_v = f.Compute.compressionRatio(raw_v, gti_v, 8)
+
+
+# Step 6: Output the results ===============================================================================+
+
+# ----------------------------------------------------------------------------------------------------------- Compilation
+
+s_g_h_list = [
+    gti_h,  # Original
+    # sg_h_1,
+    # sg_h_2,
+    # sg_h_3,
+    sg_h_11,
+    sg_h_12,
+    sg_h_13,
+]
+
+s_g_v_list = [
+    gti_v,  # Original
+    # sg_v_1,
+    # sg_v_2,
+    # sg_v_3,
+    sg_v_11,
+    sg_v_12,
+    sg_v_13,
+]
+
+s_g_h = f.Image.concatSet(s_g_h_list, axis=0)
+s_g_v = f.Image.concatSet(s_g_v_list, axis=1)
+
+if SAVE_NEW_PROCESSED:
+    f.Image.saveSet(s_g_h, "img/s_g_h.TIFF")
+    f.Image.saveSet(s_g_v, "img/s_g_v.TIFF")
+
+# ----------------------------------------------------------------------------------------------------------- Show - Information
+# Information
+print("---------- Informations ----------")
+print("raw_h image shape: ", raw_h.shape, " | Type: ", f.Image.getImageType(raw_h), " | Size: ", raw_h.size)
+print("raw_v image shape: ", raw_v.shape, " | Type: ", f.Image.getImageType(raw_v), " | Size: ", raw_v.size)
+print("gti_h image shape: ", gti_h.shape, " | Type: ", f.Image.getImageType(gti_h), " | Size: ", gti_h.size)
+print("gti_v image shape: ", gti_v.shape, " | Type: ", f.Image.getImageType(gti_v), " | Size: ", gti_v.size)
+print()
+# ----------------------------------------------------------------------------------------------------------- Show - PSNR
+print("---------- PSNR ----------")
+# Peak Signal To Noise Ratio - Original
+# Peak Signal To Noise Ratio - Smoothed
+print("PSNR GTI X Smoothed Gaussian 1 (Horizontal):", psnr_sg_h_1)
+print("PSNR GTI X Smoothed Gaussian 2 (Horizontal):", psnr_sg_h_2)
+print("PSNR GTI X Smoothed Gaussian 3 (Horizontal):", psnr_sg_h_3)
+print()
+
+# ----------------------------------------------------------------------------------------------------------- Show Compression Ratio
+print("---------- Compression Ratio ----------")
+# Compression Ratio - Scaled
+print("Compression Ratio RAW X GTI (Horizontal): ", cr_raw_gti_h)
+print("Compression Ratio Raw X GTI (Vertical): ", cr_raw_gti_v)
+print()
+
+# ----------------------------------------------------------------------------------------------------------- Show Image
+# Show Image - RAW
+# cv2.imshow("raw_h", raw_h)
+# cv2.imshow("raw_v", raw_v)
+
+# Show Image - GTI
+# cv2.imshow("gti_h", gti_h)
+# cv2.imshow("gti_v", gti_v)
+
+# Show Image - Smoothed - Gaussian
+# cv2.imshow("s_g_h", s_g_h)
+# cv2.imshow("s_g_v", s_g_v)
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 exit()
 # ----------------------------------------------------------------------------------------------------------- Show
-
-# Step 6: Apply Edge Filter ===============================================================================
-canny1 = f.EdgeFilter.applyCanny(sfBilateralRGB1, cannyParamSet[0])
-canny2 = f.EdgeFilter.applyCanny(sfBilateralRGB2, cannyParamSet[0])
-
-# Step 5: Compute Peak Signal-to-Noise Ratio (PSNR), compression ratio ======================================
-# psnr = [
-#     Compute.peakSignalToNoiseRatio(groundTruthImageSet, imgSet, 2)
-#     for imgSet in sfFullSet
-# ]
-
-# cr = [Compute.compressionRatio(groundTruthImageSet, imgSet, 2) for imgSet in sfFullSet]
-
-# Step 6: Output the results ===============================================================================+
-
-# For Smooth Filter ---------------------------------------------------------------------
-# set1 = [sfBilateralRGB1, canny1, sfBilateralRGB2, canny2]
-# set1 = cannyGraySet[IMG_NO]
-# set2 = cannySFGraySet[IMG_NO]
-# set3 = cannyRGBSet[IMG_NO]
-# set4 = cannySFRGBSet[IMG_NO]
-
-# Add reference image to the set
-# set1.insert(0, gtiGraySet[IMG_NO])
-# set2.insert(0, sfGraySet[IMG_NO])
-# set3.insert(0, gtiRGBSet[IMG_NO])
-# set4.insert(0, sfRGBSet[IMG_NO])
-
-# set1 = f.Image.concatSet(set1, axis=1)
-# set2 = f.Image.concatSet(set2, axis=1)
-# set3 = f.Image.concatSet(set3, axis=1)
-# set4 = f.Image.concatSet(set4, axis=1)
-
-# sideBySideA = f.Image.concat(set1, set3, axis=0)
-# sideBySideB = f.Image.concat(set2, set4, axis=0)
-# sideBySide = f.Image.concat(sideBySideA, sideBySideB, axis=0)
-
-# f.Image.showSet(set1, "Smooth Filter")
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
